8994fbdc4d910b81859a5fdadb9651cf1d1e017f
ValerioBob <montagliani.1759418@studenti.uniroma1.it>
2022-08-08 02:36:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Authentication microservice
+Nodejs server and mongodb database
+
+## ports
+server listen at port 3000 and db listen at port 27017
+
+## register 
+post to localhost:3000/users/register
+username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+}
+
+## login 
+post to localhost:3000/users/login
+
+username: {
+        type: String,
+        required: true,
+},
+password: {
+        type: String,
+        required: true,
+}
+
+## build:
+docker compose up --build
+
+## start / stop
+docker compose up
+docker compose down
+
---
+++ src/models/UserModels.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+    }
+
+});
+
+UserSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        //do not reveal passwordHash
+        delete returnedObject.password
+    }
+})
+
+const User =  mongoose.model("user", UserSchema);
+
+module.exports = User;
